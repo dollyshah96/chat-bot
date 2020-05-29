15309941825d5dae8447e3cea3d6a233361ca43b
Dolly <dolly@Dollys-Mac-mini.local>
2020-05-29 18:57:05 +0530

    added chat application look

--- src/app/faq-chat/faq-chat.component.ts
+++ src/app/faq-chat/faq-chat.component.ts
@@ -9,18 +9,42 @@ import * as questionJson from '../questions.json';
 export class FaqChatComponent implements OnInit {
   constructor() { }
   questions = [];
-  selectAnswer: any;
+  answers = [];
+  date = new Date();
   ngOnInit(): void {
-    console.log(questionJson);
-    setTimeout(() => {
-      const key = questionJson.data.question_id;
-      this.questions.push(questionJson.data[`${key}`]);
-    }, 2000);
+
+
+
+    const key = questionJson.data.question_id;
+    const obj = {
+      question: questionJson.data[`${key}`]['question'],
+      question_id: questionJson.data[`${key}`]['question_id'],
+      options: questionJson.data[`${key}`]['options'],
+    }
+    this.questions.push(obj);
   }
 
-  onOptionClick(option) {
-    console.log(option);
+  onOptionClick(option, question_id) {
+    this.date = new Date();
+
+    const parentIndex = this.questions.findIndex(e => e.question_id == question_id);
+    const obj = {
+      selectedOption: option,
+      questionId: question_id
+    }
+    this.answers.push(obj);
+    this.questions[parentIndex]['options'] = [];
     const key = option.optionId;
     this.questions.push(questionJson.data[`${key}`]);
+
+    // const childIndex = this.questions[parentIndex].options.findIndex(e => e.optionId == option.optionId);
+    // this.questions[parentIndex]['options'][childIndex].selectedAns = option;
+    // const removeItems = this.questions[parentIndex].options.filter(e => e.optionId != option.optionId);
+    // removeItems.map(ele => {
+    //   const childIndex = this.questions[parentIndex].options.findIndex(e => e.optionId == ele.optionId);
+    //   this.questions[parentIndex]['options'].splice(childIndex, 1);
+    // });
+
   }
 }
+
